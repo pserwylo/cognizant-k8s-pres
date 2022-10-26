6fd6081393ea2ea145890cfb6367df629847949f
Peter Serwylo <peter@serwylo.com>
2022-10-26 20:27:30 +1100

    Add backend config to make the deployment more realistic.

--- backend/routes/health.js
+++ backend/routes/health.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var config = require("../config.json");
 
 router.get('/status', function(req, res, next) {
-    res.send('{"status": "UP"}');
+    const data = {
+        status: 'UP',
+        environment: config.environment,
+    };
+
+    if (config.environment !== 'production') {
+        data.dbConnection = config.dbConnection
+    } else {
+        const connectionUrl = new URL(config.dbConnection)
+        data.dbConnection = connectionUrl.host + connectionUrl.pathname
+    }
+
+    res.json(data);
 });
 
 module.exports = router;
